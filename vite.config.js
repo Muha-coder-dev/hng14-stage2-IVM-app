e058a5e6580cd1b9b210dc0ab299f688d5ea97a3
Muha-coder-dev <muhammadsalisi123@gmail.com>
2026-04-23 18:16:23 +0100

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hng14-stage2-IVM-app/', 
+  base: '/hng14-stage2-IVM-app/',
 })
